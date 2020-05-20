f78a8980673c7a444b2d7f71a83d28937201d3e8
Maxime Bonhomme <hello.bonhomme@gmail.com>
2020-05-20 13:04:39 +0100

    add cursors

--- client/src/components/Canvas/index.js
+++ client/src/components/Canvas/index.js
@@ -1,9 +1,8 @@
-import React, { useRef, useEffect, useContext, useCallback } from "react"
+import React, { useRef } from "react"
 import styled, { css } from "@xstyled/styled-components"
-import { useMouse, useWindowSize } from "react-use"
+import { useWindowSize } from "react-use"
 
-import { UsersContext } from "../../context/UsersContext"
-import { SocketContext } from "../../context/SocketContext"
+import Cursors from "../Cursors"
 
 const full = css`
   top: 0;
@@ -22,53 +21,13 @@ const StyledCanvas = styled.canvas`
   ${full}
 `
 
-const CURSOR_SIZE = 10
-
 const Canvas = () => {
-  const { state } = useContext(UsersContext)
-  const { socket } = useContext(SocketContext)
   const canvasRef = useRef(null)
-  const ctx = useRef(null)
-  const { elX, elY } = useMouse(canvasRef)
   const { width, height } = useWindowSize()
-
-  const drawPoint = useCallback(
-    (x, y, color) => {
-      const c = ctx.current
-      c.clearRect(0, 0, width, height)
-
-      c.fillStyle = color
-      c.fillRect(x, y, CURSOR_SIZE, CURSOR_SIZE)
-    },
-    [ctx, width, height]
-  )
-
-  useEffect(() => {
-    const canvas = canvasRef.current
-    ctx.current = canvas.getContext("2d")
-  }, [canvasRef])
-
-  useEffect(() => {
-    const myselfX = elX - CURSOR_SIZE / 2
-    const myselfY = elY - CURSOR_SIZE / 2
-
-    // draw myself
-    drawPoint(myselfX, myselfY, "black")
-
-    // emit to others my position
-    socket.emit("clientMouseUpdate", {
-      ...state.myself,
-      x: elX,
-      y: elY,
-    })
-    socket.on("mouseUpdate", ({ x, y, color }) => {
-      // draw others position
-      drawPoint(x, y, color)
-    })
-  }, [elX, elY, width, height, socket, state])
 
   return (
     <Container>
+      <Cursors />
       <StyledCanvas width={width} height={height} ref={canvasRef} />
     </Container>
   )
